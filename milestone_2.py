2fe48e5eb514a0963b8a3aa8db87773caf75a2b0
Tawanda <tawanda.mafukidze@outlook.com>
2023-11-09 23:22:38 +0000

    First Commit

---
+++ milestone_2.py
@@ -0,0 +1,15 @@
+import random
+
+word_list = ["tangerine", "apple", "pineapple", "plum", "banana"]
+
+word = random.choice(word_list)
+
+print(word)
+
+guess = input(str("Enter a letter: "))
+
+if len(guess) == 1 and guess.isalpha():
+    print("Good Guess")
+else:
+    print("Oops! That is not a valid input.")
+
