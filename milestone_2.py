44a81804fdbc5c7a12c7b415fe48eaad5655bb9b
Tawanda Mafukidze <116578809+Tawanda23@users.noreply.github.com>
2023-11-16 10:59:22 +0000

    Update milestone_2.py

--- milestone_2.py
+++ milestone_2.py
@@ -1,13 +1,9 @@
 import random
-
 word_list = ["tangerine", "apple", "pineapple", "plum", "banana"]
-
 word = random.choice(word_list)
-
 print(word)
 
 guess = input(str("Enter a letter: "))
-
 if len(guess) == 1 and guess.isalpha():
     print("Good Guess")
 else:
